4be1503f85ee3946ba2818b9431256fd374e137b
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-06-24 21:24:44 +0530

    Add Basic Functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,74 @@
 <template>
   <main class="max-w-lg mx-auto px-6">
     <add-task @newTask="getTasks" />
+
+    <div id="searchTaskBar">
+      <aside class="mx-auto flex justify-between mt-2 px-4">
+        <label for="search task" class="flex-1">
+          <input
+            type="text"
+            name="search task"
+            v-model.trim="searchTodo"
+            class="
+              todo-search-task-input
+              px-4
+              py-2
+              placeholder-yellowGray-300
+              text-yellowGray-600
+              bg-white
+              rounded
+              text-sm
+              border border-yellowGray-300
+              outline-none
+              focus:outline-none focus:ring
+              w-full
+            "
+            placeholder="Filter tasks"
+          />
+        </label>
+      </aside>
+    </div>
+
     <transition>
-      <span v-if="loading">Fetching Tasks....</span>
+      <div v-if="loading">
+        <br />
+        <span
+          class="
+            border
+            flex
+            border-gray-500
+            rounded
+            px-2
+            py-2
+            justify-between
+            items-center
+            mb-2
+          "
+        >
+          Fetching Tasks....
+        </span>
+      </div>
+      <div v-else-if="todos.length == 0">
+        <br />
+        <span
+          class="
+            border
+            flex
+            border-gray-500
+            rounded
+            px-2
+            py-2
+            justify-between
+            items-center
+            mb-2
+          "
+        >
+          No Todos Have been Added
+        </span>
+      </div>
       <ul v-else class="flex-col mt-9 mx-auto">
         <li
-          v-for="(todo, index) in todos"
+          v-for="(todo, index) in filteredTodos"
           :key="todo.id"
           class="
             border
@@ -21,19 +84,20 @@
         >
           <label :for="todo.id">
             <input
-              :id="todo.id"
               type="text"
+              :id="todo.id"
               :class="[
-                'hideme appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring todo-edit-task-input',
+                'appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring todo-edit-task-input',
               ]"
               :name="todo.title"
-              placeholder="Edit The Task"
+              v-model.trim="todos[index].updatedValue"
+              v-show="todos[index].editMode"
+              placeholder="Enter the task here!"
             />
           </label>
           <div class="">
             <button
               class="
-                hideme
                 bg-transparent
                 hover:bg-gray-500
                 text-gray-700 text-sm
@@ -46,15 +110,19 @@
                 todo-update-task
               "
               type="button"
+              v-show="todos[index].editMode"
               @click="updateTask(index, todo.id)"
             >
               Done
             </button>
           </div>
-          <div :class="['todo-task text-gray-600']">
+          <div
+            :class="['todo-task text-gray-600']"
+            v-show="!todos[index].editMode"
+          >
             {{ todo.title }}
           </div>
-          <span class="">
+          <span v-show="!todos[index].editMode">
             <button
               style="margin-right: 5px"
               type="button"
@@ -108,64 +176,117 @@ import { defineComponent } from '@nuxtjs/composition-api'
 import addTask from '~/components/addTask.vue'
 
 export default defineComponent({
+  middleware: 'auth',
   components: { addTask },
+
   data() {
     return {
-      hello: 'hello world!',
-      todos: [
-        {
-          title: 'Henlo',
-          id: 1,
-          editing: false,
-        },
-        {
-          title: 'Frens',
-          id: 2,
-          editing: false,
-        },
-      ],
+      searchTodo : '',
+      todos: [],
       loading: false,
     }
   },
-  mounted() {
-    this.getTasks()
+
+  computed: {
+    filteredTodos() {
+      let filter = new RegExp(this.searchTodo, 'i')
+      return this.todos.filter(el => el.title.match(filter))
+    }
   },
+
+  mounted() {
+    this.loading = true
+    this.getTasks()
+    this.$toast.info('Fetching your Todos...')
+  },
+
   methods: {
     async getTasks() {
-      /***
-       * @todo Fetch the tasks created by the user and display them.
-       * @todo also the function to display a single new task added
-       * @hints use store and set loading true
-       * @caution you have to assign new value to todos for it to update
-       */
+      const headersForAuthorization = {
+        Authorization: 'Token ' + this.$store.getters.token,
+      }
+
+      this.$axios({
+        headers: headersForAuthorization,
+        url: 'todo/',
+        method: 'get',
+      })
+        .then((res) => {
+          res.data.forEach(function (todo) {
+            todo.editMode = false
+            todo.updatedValue = todo.title
+          })
+          this.todos = res.data
+          this.loading = false
+        })
+        .catch((err) =>
+          $toast.error(
+            "Can't fetch data right now. Please reload to try again."
+          )
+        )
     },
-    /**
-     * Function to update a single todo
-     * @argument {number} _index - index of element to update in todos array
-     * @argument {number} _id - id of todo obtained from API
-     * @todo Complete this function.
-     * @todo 1. Send the request to update the task to the backend server.
-     * @todo 2. Update the task in the dom.
-     */
-    updateTask(_index, _id) {},
-    /**
-     * toggle visibility of input and buttons for a single todo
-     * @argument {number} index - index of element to toggle
-     * @todo add in bindings in dom so that 'hideme' class is dynamic or use conditional rendering
-     * @hint read about class bindings in vue
-     */
-    editTask(index) {
-      this.todos[index].editing = !this.todos[index].editing
+
+    updateTask(_index, _id) {
+      var newTodo = this.todos[_index].updatedValue
+
+      if (newTodo === '') {
+        this.$toast.error('Todo value cannot be blank. ')
+        return
+      }
+
+      if (newTodo === this.todos[_index].title) {
+        this.$toast.info('No change in the value of the todo.')
+        this.editTask(_index)
+        return
+      }
+
+      const headersForAuthorization = {
+        Authorization: 'Token ' + this.$store.getters.token,
+      }
+      const dataForAPIRequest = {
+        title: newTodo,
+      }
+
+      this.$toast.info('Updating your todo....')
+      this.$axios({
+        headers: headersForAuthorization,
+        url: 'todo/' + _id + '/',
+        method: 'patch',
+        data: dataForAPIRequest,
+      })
+        .then(() => {
+          this.$toast.success('Todo updated successfully!')
+          this.todos[_index].title = newTodo
+          this.editTask(_index)
+        })
+        .catch(() =>
+          this.$toast.error('Updation failed. Please try again later.')
+        )
     },
-    /**
-     * Function to delete a single todo
-     * @argument {number} _index - index of element to update in todos array
-     * @argument {number} _id - id of todo obtained from API
-     * @todo Complete this function.
-     * @todo 1. Send the request to delete the task to the backend server.
-     * @todo 2. Remove the task from the dom.
-     */
-    deleteTask(_index, _id) {},
+
+    editTask(_index) {
+      this.todos[_index].editMode = !this.todos[_index].editMode
+    },
+
+    deleteTask(_index, _id) {
+      const headersForAuthorization = {
+        Authorization: 'Token ' + this.$store.getters.token,
+      }
+
+      this.$toast.info('Deleting your todo....')
+      this.$axios({
+        headers: headersForAuthorization,
+        url: 'todo/' + _id + '/',
+        method: 'delete',
+      })
+        .then(() => {
+          this.$toast.success('Todo deleted successfully!')
+          this.todos.splice(_index, 1)
+        })
+        .catch(() =>
+          this.$toast.error('Deletion failed. Please try again later.')
+        )
+    },
   },
 })
 </script>
