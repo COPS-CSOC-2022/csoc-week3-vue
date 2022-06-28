4e561c26336f9ffb1554ae21a2405b0e315d8392
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2022-06-28 19:14:00 +0530

    Completed Middleware function

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,9 @@
 import { defineNuxtMiddleware } from '@nuxtjs/composition-api'
 
 export default defineNuxtMiddleware((context) => {
-  /***
-   * @todo Redirect the user to login page if token is not present in store.
-   * @todo Redirect the user to main page if token is present in store
-   * @hints check what propeties context has
-   */
+  let path = context.route.fullPath;
+  let token = context.store.getters.token;
+
+  if (token === null && path === '/') context.redirect('login/');
+  else if (token != null && path != '/') context.redirect('/'); 
 })
